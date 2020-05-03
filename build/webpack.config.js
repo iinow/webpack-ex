579601f6300df95b5b821ee7917dfacdcb296d76
iinow <iinow@naver.com>
2020-05-04 08:13:45 +0900

    add typescript webpack config

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,4 +1,3 @@
-const { Plugin, Compiler } = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
@@ -7,6 +6,36 @@ const ManifestPlugin = require('webpack-manifest-plugin')
 /**
  * webpack 은 js, json 만 읽을 수 있는데 Loader 를 통해서 다른 언어, 파일 등을 읽을 수 있다...
  * */
+console.log(ddd)
+// configuration = {
+//   mode: 'production',
+//   entry: {
+//     app: './src/index.js',
+//   },
+//   output: {
+//     filename: '[name].bundle.js',
+//     path: path.resolve(__dirname, '../dist'),
+//     publicPath: path.resolve(__dirname, '../dist'),
+//     chunkFilename: '[name].bundle.js'
+//   },
+//   plugins: [
+//     new ManifestPlugin(),
+//     new CleanWebpackPlugin({
+//       cleanStaleWebpackAssets: true
+//     }),   //빌드 된 파일이 전에 빌드된 파일이면 제거한다 -> 항상 최신 빌드 파일들만 모인다.
+//     new HtmlWebpackPlugin({
+//       // template: path.join(__dirname, 'src/index.html'),
+//       title: 'Output Management',
+//       // inject: false,
+//       filename: path.join(__dirname, '../dist/index.html')
+//     })
+//   ],
+//   devServer: {
+//     contentBase: path.join(__dirname, '../dist'),
+//     hot: true
+//   },
+//   devtool: 'source-map',
+// }
 
 module.exports = {
   mode: 'production', //mode 의 default 값은 production 이다
@@ -20,11 +49,6 @@ module.exports = {
     publicPath: path.resolve(__dirname, '../dist'),
     chunkFilename: '[name].bundle.js'
   },
-  // optimization: {
-  //   splitChunks: {
-  //     chunks: 'all'
-  //   }
-  // },
   plugins: [
     new ManifestPlugin(),
     new CleanWebpackPlugin({
---
+++ build/webpack.local.config.ts
@@ -0,0 +1,41 @@
+import * as path from 'path'
+import * as webpack from 'webpack'
+// @ts-ignore
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+import ManifestPlugin from 'webpack-manifest-plugin'
+
+/**
+ * webpack 은 js, json 만 읽을 수 있는데 Loader 를 통해서 다른 언어, 파일 등을 읽을 수 있다...
+ * */
+let config: webpack.Configuration = {
+  mode: 'production',
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: path.resolve(__dirname, '../dist'),
+    chunkFilename: '[name].[hash].js'
+  },
+  plugins: [
+    new ManifestPlugin(),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: true
+    }),   //빌드 된 파일이 전에 빌드된 파일이면 제거한다 -> 항상 최신 빌드 파일들만 모인다.
+    new HtmlWebpackPlugin({
+      // template: path.join(__dirname, 'src/index.html'),
+      title: 'Output Management',
+      // inject: false,
+      filename: path.join(__dirname, '../dist/index.html')
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, '../dist'),
+    hot: true,
+  },
+  devtool: 'source-map'
+}
+
+module.exports = config

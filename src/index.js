034bd979f3d5d154ead4b1e675ffea5a44225c29
iinow <iinow@naver.com>
2020-05-03 22:12:10 +0900

    output management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import { join } from 'lodash-es'
+import printMe from './print'
 
 function component() {
   const element = document.createElement('div')
+  const btn = document.createElement('button')
 
   element.innerHTML = join(['Hello', 'webpack'], ' ')
+
+  btn.innerText = 'Click me and check the console'
+  btn.onclick = printMe
+
+  element.appendChild(btn)
 
   return element
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const { Plugin, Compiler } = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
 
 /**
  * webpack 은 js, json 만 읽을 수 있는데 Loader 를 통해서 다른 언어, 파일 등을 읽을 수 있다...
@@ -9,13 +11,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'production', //mode 의 default 값은 production 이다
   entry: {
-    app: './src/index.js'
+    app: './src/index.js',
+    print: './src/print.js'
   },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  // plugins: [
-  //   new HtmlWebpackPlugin()
-  // ]
+  plugins: [
+    new ManifestPlugin(),
+    new CleanWebpackPlugin(),   //빌드 된 파일이 전에 빌드된 파일이면 제거한다 -> 항상 최신 빌드 파일들만 모인다.
+    new HtmlWebpackPlugin({
+      // template: path.join(__dirname, 'src/index.html'),
+      title: 'Output Management',
+      // inject: false,
+      // filename: path.join(__dirname, 'dist/index.html')
+    })
+  ],
+  devtool: "source-map"
 }

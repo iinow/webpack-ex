cb24192e3d503079cd9bbf77f444d2756ea47a26
iinow <iinow@naver.com>
2020-05-30 15:21:21 +0900

    eslint plugin 예제 추가

---
+++ plugins/my-babel-plugin.ts
@@ -0,0 +1,24 @@
+module.exports = function myBabelPlugin() {
+    return {
+        visitor: {
+            // Identifier(path) {
+            //     const name = path.node.name
+            //
+            //     console.log('Identifier() name: ', name)
+            //
+            //     //변환 작업
+            //     path.node.name = name
+            //         .split("")
+            //         .reverse()
+            //         .join("")
+            // },
+            VariableDeclaration(path) {
+                console.log('VariableDeclaration() kind: ', path.node.kind)
+
+                if (path.node.kind === 'const') {
+                    path.node.kind = 'var'
+                }
+            }
+        }
+    }
+}
--- src/print.js
+++ src/print.js
@@ -1,3 +1,5 @@
 export default function print(text) {
   console.log(text)
+  const hell = 'hell...'
+  console.log(hell)
 }
